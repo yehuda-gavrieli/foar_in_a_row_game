5f406a4e6692c238c841ebdb376b8c36d4bb2924
Yehuda gavrieli <yehuda8009@gmail.com>
2026-05-12 10:25:51 +0300

    create 2 function 1 create board 2 print board

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def create_board():
+    rows = 6
+    cols = 7
+    board = []
+    for _ in range(rows):
+        rows = [" "] * cols
+        board.append(rows)
+    return board
+
+def print_board(board): 
+    print("---b o a r d---")
+    for rows in board:
+        print("|" + "|".join(rows) + "|")
+    print("_" * 15)
+
+print_board(create_board())
